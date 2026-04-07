cb29ce2b550845cc226448c7c1728436844cc1b0
CommeAvant1 <omarfax135@gmail.com>
2026-04-06 23:09:11 -0500

    avances hasta antes del probla de funciones

---
+++ FlujoControl.py
@@ -0,0 +1,19 @@
+name = 'gabriel'
+
+if name == 'gabriel':
+    print('hola gabriel')
+elif name == 'lamine':
+    print ('hola lamine')
+else:
+    print ('¿quien eres?')
+
+
+
+spam = 0
+while spam < 5:  # Continue while spam is less than 5
+    print('Hello, world.')
+    spam = spam + 1  # Increment counter to avoid infinite loop
+
+
+for i in range(5):
+    print(f'Will stop at 5! or 4? ({i})')  
--- problema1.py
+++ problema1.py
@@ -3,4 +3,22 @@ pida el nombre del cliente ,
 cuantas manzanos comprara y un precio. 
 muestre el nombre y el total a pagar
 """
+precio : float = 0.50
 
+print("INGRESE SU NOMBRE")
+nombre = input()
+
+print("¿cuantas manzanas va a comprar?")
+nmrDeManzanas = int(input())
+
+costo = precio * nmrDeManzanas
+
+print(f"se llama {nombre}, quiere comprar {nmrDeManzanas} manzanas y el el costo total es de {costo}")
+
+"""
+nom = input("Ingrese nombre:\n")
+can = int(input("Ingrese cantidad manzanas: \n"))
+pre = float(input("Ingrese precio:\n"))
+tot = can pre
+print(f"(nom) compro S/{tot}")
+"""
